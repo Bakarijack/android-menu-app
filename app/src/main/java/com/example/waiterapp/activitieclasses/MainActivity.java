87df94edb851f3f3c1e8c40c6d43ead7c7c8c5c7
Bakarijack <bakarikilu254@gmail.com>
2023-05-28 12:30:57 +0300

    spinner and login page complete

--- app/src/main/java/com/example/waiterapp/activitieclasses/MainActivity.java
+++ app/src/main/java/com/example/waiterapp/activitieclasses/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.waiterapp.activitieclasses;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.waiterapp.R;
 
@@ -11,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //get the action bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        //create a full screen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
     }
 }
