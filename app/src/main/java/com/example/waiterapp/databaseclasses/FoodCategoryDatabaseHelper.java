44d9da7e2313c58a0c5f3dac5f4a699d7931d5ec
Bakarijack <bakarikilu254@gmail.com>
2023-05-30 09:46:54 +0300

    adding insert query

--- app/src/main/java/com/example/waiterapp/databaseclasses/FoodCategoryDatabaseHelper.java
+++ app/src/main/java/com/example/waiterapp/databaseclasses/FoodCategoryDatabaseHelper.java
@@ -1,9 +1,38 @@
 package com.example.waiterapp.databaseclasses;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 public class FoodCategoryDatabaseHelper extends DatabaseHelper{
     public FoodCategoryDatabaseHelper(Context context) {
         super(context);
+    }
+
+    public boolean isCategoryExist(String categoryN){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from foodcategory where reviewDate = '"+categoryN+"'",null);
+
+        if (cursor.getCount() > 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean insertCategory(String categoryN){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put("categoryName",categoryN);
+
+        long result = db.insert("foodcategory",null,contentValues);
+
+        if (result == -1){
+            return false;
+        }else {
+            return true;
+        }
     }
 }
