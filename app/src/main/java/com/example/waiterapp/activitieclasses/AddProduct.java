2b4932604e3d9b8c5e367e1f4095f8f65be3a0d8
Bakarijack <bakarikilu254@gmail.com>
2023-05-30 09:03:12 +0300

    image picker working ok

--- app/src/main/java/com/example/waiterapp/activitieclasses/AddProduct.java
+++ app/src/main/java/com/example/waiterapp/activitieclasses/AddProduct.java
@@ -1,16 +1,54 @@
 package com.example.waiterapp.activitieclasses;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.waiterapp.R;
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AddProduct extends AppCompatActivity {
+    private FloatingActionButton photoFab;
+    private CircleImageView foodImage;
+
+
+    private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
+
+        photoFab = (FloatingActionButton) findViewById(R.id.photoFab);
+        foodImage = (CircleImageView) findViewById(R.id.foodImage);
+
+        photoFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imagePickerAction();
+            }
+        });
+    }
+
+    public void imagePickerAction(){
+        ImagePicker.with(this)
+                .crop()	    			//Crop image(Optional), Check Customization for more option
+                .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                .start();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uri = data.getData();
+        foodImage.setImageURI(uri);
     }
 }
